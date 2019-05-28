3a662a93c5c22db6ce85bfc278381b08cf2d5677
x-Janek, Andras Szabolcs <x-AndrasSzabolcs.Janek@wizzair.com>
2019-05-28 15:28:56 +0200

    horizon zero dawn

--- navigator.js
+++ navigator.js
@@ -46,7 +46,7 @@ function deviceOrientationHandler(gamma, beta, dir) {
 
     //var isInLandscape = false; Math.abs(gamma) > Math.abs(beta);
 
-    var horizonRotation = beta;
+    var horizonRotation = -beta;
 
 
     var horizon = document.getElementById("horizon");
