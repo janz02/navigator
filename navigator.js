be21d257d2b4b495ff48354a4906e72798dd3707
x-Janek, Andras Szabolcs <x-AndrasSzabolcs.Janek@wizzair.com>
2019-05-30 15:47:34 +0200

    share

--- navigator.js
+++ navigator.js
@@ -1,6 +1,6 @@
-var hFoV = 90;
+﻿var hFoV = 90;
 var vFoV = 40;
-
+var pos = {};
 
 function manual() {
     deviceOrientationHandler(
@@ -15,9 +15,40 @@ function getLocation() {
     }
 }
 function showPosition(position) {
-    document.getElementById("lat").innerHTML = position.coords.latitude;
-    document.getElementById("lon").innerHTML = position.coords.longitude;
-    document.getElementById("alt").innerHTML = position.coords.altitude;
+
+    pos = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+        alt: position.coords.altitude
+    };
+
+    document.getElementById("lat").innerHTML = pos.lat;
+    document.getElementById("lon").innerHTML = pos.lon;
+    document.getElementById("alt").innerHTML = pos.alt;
+
+    if (document.URL.indexOf('?') > 0) {
+        var objCoords = document.URL.substring(document.URL.indexOf('?') + 1).split(',')
+        var obj = {
+            lat: objCoords[0],
+            lon: objCoords[1],
+            alt: objCoords[2]
+        };
+
+        calculateDirection(obj);
+
+        console.log("Célpont " + Math.floor(obj.dir) + "° " + Math.floor(obj.dist * 1000) + "m");
+    }
+
+
+    //Calculate Sight directions
+
+    for (var i = 0; i < sights.length; i++) {
+        var sight = sights[i];
+        calculateDirection(sight);
+
+        console.log(sight.text + " " + Math.floor(sight.dir) + "° " + Math.floor(sight.dist * 1000) + "m");
+
+    }
 }
 
 function openCamera() {
@@ -153,3 +184,125 @@ document.addEventListener("DOMContentLoaded", function (event) {
             false);
     };
 });
+
+
+function calculateDirection(obj) {
+    var latDiff = obj.lat - pos.lat;
+    var lonDiff = obj.lon - pos.lon;
+
+    var sightDir = 90 + Math.atan(latDiff / lonDiff) * (180 / 3.14);
+
+    if (lonDiff > 0) {
+        sightDir += 180;
+    }
+
+    obj.dir = sightDir;
+    obj.dist = distanceInKmBetweenEarthCoordinates(obj.lat, obj.lon, pos.lat, pos.lon);
+}
+
+function degreesToRadians(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+    var earthRadiusKm = 6371;
+
+    var dLat = degreesToRadians(lat2 - lat1);
+    var dLon = degreesToRadians(lon2 - lon1);
+
+    lat1 = degreesToRadians(lat1);
+    lat2 = degreesToRadians(lat2);
+
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return earthRadiusKm * c;
+}
+
+var sights =
+    [
+        {
+            text: "János hegy",
+            lat: 47.518053,
+            lon: 18.960178,
+            alt: 500
+        },
+        {
+            text: "Citadella",
+            lat: 47.486944,
+            lon: 19.047011,
+            alt: 230
+        },
+        {
+            text: "Bazilika",
+            lat: 47.500801,
+            lon: 19.053814,
+            alt: 170
+        },
+        {
+            text: "Hármashatárhegy",
+            lat: 47.555880,
+            lon: 19.002418,
+            alt: 500
+        },
+        {
+            text: "Telekom adótorony",
+            lat: 47.468714,
+            lon: 19.126104,
+            alt: 130
+        },
+        {
+            text: "Lakihegyi adótorony",
+            lat: 47.373155,
+            lon: 19.004955,
+            alt: 420
+        },
+        {
+            text: "Liszt Ferenc reptér",
+            lat: 47.435109,
+            lon: 19.253196,
+            alt: 120
+        },
+        {
+            text: "Kékes tetö",
+            lat: 47.871354,
+            lon: 20.011534,
+            alt: 1014
+        },
+    ];
+
+
+function share() {
+    var url = document.querySelector('link[rel=canonical]')
+        ? document.querySelector('link[rel=canonical]').href
+        : window.location.href;
+
+    if (url.indexOf('?') > 0) {
+        url = url.substring(0, url.indexOf('?'));
+    }
+
+    url += "?" + pos.lat + "," + pos.lon
+
+    if (alt.length > 0) {
+        url += "," + pos.alt
+    }
+    var copyText = document.getElementById("clipboard");
+
+    copyText.value = url;
+
+    if (navigator.share) {
+        navigator.share({
+            title: "Navigator position share",
+            text: 'With this page you can check where am I',
+            url: url
+        })
+            .then(() => console.log('Successfully shared'))
+            .catch((error) => console.log('Error sharing:', error));
+    } else  {
+
+        copyText.select();
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+    }
+
+}
